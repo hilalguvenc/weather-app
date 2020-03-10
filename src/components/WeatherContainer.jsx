6eed851e1aac1c7729c6066822af0e57032012de
hlalgvnc <hilalguvenc@anadolu.edu.tr>
2020-03-10 14:23:25 +0300

    updated

--- src/components/WeatherContainer.jsx
+++ src/components/WeatherContainer.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { format, addDays } from "date-fns";
 
 function WeatherContainer() {
   const [countries, setCountries] = useState([]);
   const [coordinates, setCoordinates] = useState([]);
   const [lat, setLat] = useState(0);
   const [lon, setLon] = useState(0);
+  const [dailyWeather, setDailyWeather] = useState(0);
+  const [weeklyWeather, setWeeklyWeather] = useState(0);
+  const [startDate, setStartDate] = useState(0);
 
   const getCountries = async () => {
     const response = await fetch(`https://restcountries.eu/rest/v2/all`);
@@ -18,18 +22,27 @@ function WeatherContainer() {
     );
     const json = await response.json();
     setCoordinates(json.results);
-    const lat = json.results[2].geometry.lat;
-    const lon = json.results[2].geometry.lng;
+    const lat = json.results[0].geometry.lat;
+    const lon = json.results[0].geometry.lng;
     setLat(lat);
     setLon(lon);
     console.log(lat);
     console.log(lon);
   };
+  const getDailyWeather = async () => {
+    const strtdate = format(startDate, "yyyy-MM-dd");
+    const enddt = format(addDays(startDate, 1), "yyyy-MM-dd");
+    const response = await fetch(
+      `https://api.weatherbit.io/v2.0/history/daily?lat=${lat}&lon=${lon}&start_date=${strtdate}&end_date=${enddt}&key=a317f6a1df6846e4b668ce089528ba71`
+    );
+    const json = await response.json();
+    setDailyWeather(json);
+    console.log(json);
+  };
   useEffect(() => {
     getCountries();
-  }, []);
-  useEffect(() => {
     getCoordinates();
+    getDailyWeather();
   }, []);
   return (
     <div className="weather-container">
